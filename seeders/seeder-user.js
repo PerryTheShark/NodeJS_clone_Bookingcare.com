849d94e4bdd2577cff6112457d16adf48de85c02
BerryTheShark <tungtv.uet@gmail.com>
2023-03-09 17:01:52 +0700

    code_09/03/2023

--- seeders/seeder-user.js
+++ seeders/seeder-user.js
@@ -4,9 +4,14 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Users", [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "example@example.com",
+        email: "admin@gmail.com",
+        password: "123456",
+        firstName: "Perry",
+        lastName: "TheShark",
+        address: "USA",
+        gender: 1,
+        typeRole: "ROLE",
+        keyRole: "R1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
