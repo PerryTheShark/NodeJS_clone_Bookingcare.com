7bf1247286417161b81344cd09ebb4891542ecc7
BerryTheShark <tungtv.uet@gmail.com>
2023-03-28 11:23:51 +0700

    28/3/2023 add userService.js

---
+++ src/services/userService.js
@@ -0,0 +1,68 @@
+import db from "../models/index";
+import bcrypt from "bcryptjs";
+
+let handleUserLogin = (email, password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let userData = {};
+
+      let isExist = await checkUserEmail(email);
+      if (isExist) {
+        //user already exist
+
+        let user = await db.User.findOne({
+          attributes: ["email", "roleId", "password"],
+          where: { email: email },
+          raw: true,
+        });
+        if (user) {
+          //compare password
+          let check = await bcrypt.compareSync(password, user.password); // false
+          if (check) {
+            userData.errCode = 0;
+            userData.errMessage = "ok";
+            console.log("user", user);
+            delete user.password;
+            userData.user = user;
+          } else {
+            userData.errCode = 3;
+            userData.errMessage = "wrong password";
+          }
+        } else {
+          userData.errCode = 2;
+          userData.errMessage = "User's not found";
+        }
+      } else {
+        //return err
+        userData.errCode = 1;
+        userData.errMessage =
+          "Your's Email isn't exits in your system. please try another email!";
+      }
+
+      resolve(userData);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+let checkUserEmail = (userEmail) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { email: userEmail },
+      });
+      if (user) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = {
+  handleUserLogin: handleUserLogin,
+};
