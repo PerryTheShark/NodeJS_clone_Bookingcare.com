128dc247a726ce94f6852028d7ab98039d9072a5
PeryTheShark <tungtv.uet@gmail.com>
2023-05-18 11:40:56 +0700

    #63 change migration, change key to kewMap

--- src/models/allcode.js
+++ src/models/allcode.js
@@ -8,12 +8,17 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      Allcode.hasMany(models.User, {
+        foreignKey: "positionId",
+        as: "positionData",
+      });
+      Allcode.hasMany(models.User, { foreignKey: "gender", as: "genderData" });
       // define association here
     }
   }
   Allcode.init(
     {
-      key: DataTypes.STRING,
+      keyMap: DataTypes.STRING,
       type: DataTypes.STRING,
       valueEn: DataTypes.STRING,
       valueVi: DataTypes.STRING,
--- src/models/user.js
+++ src/models/user.js
@@ -8,6 +8,16 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      User.belongsTo(models.Allcode, {
+        foreignKey: "positionId",
+        targetKey: "keyMap",
+        as: "positionData",
+      });
+      User.belongsTo(models.Allcode, {
+        foreignKey: "gender",
+        targetKey: "keyMap",
+        as: "genderData",
+      });
       // define association here
     }
   }
