6d3e2d056b44ba27b56d1a38c16a141e74529941
PeryTheShark <tungtv.uet@gmail.com>
2023-05-29 12:08:39 +0700

    convert image to base64 on server #69

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -96,7 +96,7 @@ let getDetailDoctorById = (inputId) => {
             id: inputId,
           },
           attributes: {
-            exclude: ["password", "image"],
+            exclude: ["password"],
           },
           include: [
             {
@@ -109,9 +109,16 @@ let getDetailDoctorById = (inputId) => {
               attributes: ["valueEn", "valueVi"],
             },
           ],
-          raw: true,
+          raw: false,
           nest: true,
         });
+
+        if (data && data.image) {
+          data.image = new Buffer(data.image, "base64").toString("binary");
+        }
+
+        if (!data) data = {};
+
         resolve({
           errCode: 0,
           data: data,
