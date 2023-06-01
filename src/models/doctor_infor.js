e7380fccc5c391244031755a98f1b21db02db510
PeryTheShark <tungtv.uet@gmail.com>
2023-06-01 14:23:03 +0700

    add model and migration doctor_infor

---
+++ src/models/doctor_infor.js
@@ -0,0 +1,32 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class doctor_infor extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  doctor_infor.init(
+    {
+      doctorId: DataTypes.INTEGER,
+      priceId: DataTypes.STRING,
+      provinceId: DataTypes.STRING,
+      paymentId: DataTypes.STRING,
+      addressClinic: DataTypes.STRING,
+      nameClinic: DataTypes.STRING,
+      note: DataTypes.STRING,
+      count: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "doctor_infor",
+    }
+  );
+  return doctor_infor;
+};
