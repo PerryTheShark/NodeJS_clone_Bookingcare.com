a438a07c265b2478aa5dda68c46ee56948b8cdb8
PeryTheShark <tungtv.uet@gmail.com>
2023-06-06 15:58:33 +0700

    add link token verify booking appointment

--- src/route/web.js
+++ src/route/web.js
@@ -10,11 +10,9 @@ let initWebRoutes = (app) => {
   router.get("/", homeController.getHomePage);
   router.get("/about", homeController.getAboutPage);
   router.get("/crud", homeController.getCRUD);
-
   router.post("/post-crud", homeController.postCRUD);
   router.get("/get-crud", homeController.displayGetCRUD);
   router.get("/edit-crud", homeController.getEditCRUD);
-
   router.post("/put-crud", homeController.putCRUD);
   router.get("/delete-crud", homeController.deleteCRUD);
 
@@ -23,24 +21,20 @@ let initWebRoutes = (app) => {
   router.post("/api/create-new-user", userController.handleCreateNewUser);
   router.put("/api/edit-user", userController.handleEditUser);
   router.delete("/api/delete-user", userController.handleDeleteUser);
-
   router.get("/api/allcode", userController.getAllCode);
 
   router.get("/api/top-doctor-home", doctorController.getTopDoctorHome);
-
   router.get("/api/get-all-doctors", doctorController.getAllDoctors);
   router.post("/api/save-infor-doctor", doctorController.postInforDoctor);
   router.get(
     "/api/get-detail-doctor-by-id",
     doctorController.getDetailDoctorById
   );
-
   router.post("/api/bulk-create-schedule", doctorController.bulkCreateSchedule);
   router.get(
     "/api/get-schedule-doctor-by-date",
     doctorController.getScheduleByDate
   );
-
   router.get(
     "/api/get-extra-infor-doctor-by-id",
     doctorController.getExtraInforDoctorById
@@ -53,6 +47,10 @@ let initWebRoutes = (app) => {
   router.post(
     "/api/patient-book-appointment",
     patientController.postBookAppointment
+  );
+  router.post(
+    "/api/verify-book-appointment",
+    patientController.postVerifyBookAppointment
   );
 
   return app.use("/", router);
--- src/services/patientService.js
+++ src/services/patientService.js
@@ -1,6 +1,12 @@
 import db from "../models/index";
 require("dotenv").config();
 import emailService from "./emailService";
+import { v4 as uuidv4 } from "uuid";
+
+let buildUrlEmail = (token, doctorId) => {
+  let result = `${process.env.URL_REACT}/verify-booking?token=${token}&doctorId=${doctorId}`;
+  return result;
+};
 
 let postBookAppointment = (data) => {
   return new Promise(async (resolve, reject) => {
@@ -17,13 +23,14 @@ let postBookAppointment = (data) => {
           errMessage: "Missing parameter",
         });
       } else {
+        let token = uuidv4();
         await emailService.sendSimpleEmail({
           receiverEmail: data.email,
           patientName: data.fullName,
           time: data.timeString,
           doctorName: data.doctorName,
           language: data.language,
-          redirectLink: "https://www.youtube.com/",
+          redirectLink: buildUrlEmail(token, data.doctorId),
         });
 
         //upsert
@@ -45,6 +52,7 @@ let postBookAppointment = (data) => {
               patientId: user[0].id,
               date: data.date,
               timeType: data.timeType,
+              token: token,
             },
           });
         }
@@ -59,6 +67,45 @@ let postBookAppointment = (data) => {
   });
 };
 
+let postVerifyBookAppointment = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.token || !data.doctorId) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing parameter",
+        });
+      } else {
+        let appointment = await db.Booking.findOne({
+          where: {
+            doctorId: data.doctorId,
+            token: data.token,
+            statusId: "S1",
+          },
+          raw: false,
+        });
+
+        if (appointment) {
+          appointment.statusId = "S2";
+          await appointment.save();
+          resolve({
+            errCode: 0,
+            errMessage: "Update the appointment succeed!",
+          });
+        } else {
+          resolve({
+            errCode: 2,
+            errMessage: "Appointment have been active or not valid!",
+          });
+        }
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
 module.exports = {
   postBookAppointment: postBookAppointment,
+  postVerifyBookAppointment: postVerifyBookAppointment,
 };
